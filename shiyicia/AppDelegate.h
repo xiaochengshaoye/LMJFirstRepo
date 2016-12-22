02f1ae3a6c6417a8c3f8f5bd877a4a9bced36afc
刘明杰 <liumingjie@liumingjiedeimac.local>
2016-12-22 11:07:12 +0800

    Initial Commit

---
+++ shiyicia/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  shiyicia
+//
+//  Created by 刘明杰 on 16/12/22.
+//  Copyright © 2016年 刘明杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ shiyicia/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  shiyicia
+//
+//  Created by 刘明杰 on 16/12/22.
+//  Copyright © 2016年 刘明杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
